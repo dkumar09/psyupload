c284a4798a90cfce3f1e6f4b31aa55ea6d5c6e5d
dkumar09 <onlydeepender@gmail.com>
2019-12-28 00:45:27 +0530

    Added upload and qrCode

---
+++ include/Uploader.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var formidable = require('formidable');
+module.exports=function(req,res,callback){
+    var form  = new formidable.IncomingForm({uploadDir:__dirname+'/../tmp',keepExtensions:true});
+    form.parse(req,(err,fields,files)=>{
+        if(err){
+            callback('01');
+        }
+        else{
+            console.log(files.uploadedFile.name);
+            var oldPath=files.uploadedFile.path;
+            newPath = './uploads/'+files.uploadedFile.name;
+            if(fs.existsSync('./uploads/'+files.uploadedFile.name)){
+                newPath = './uploads/copy_of_'+files.uploadedFile.name;
+            }
+            fs.rename(oldPath,newPath,(err)=>{
+                console.log(err);
+            })
+            callback('02');
+        };
+
+    });
+}
--- server.js
+++ server.js
@@ -1,6 +1,39 @@
+'use strict';
+const port = 3000;
+var dns = require('dns');
+var QRCode = require('qrcode')
 var express = require('express');
+var opn = require('opn')
+var uploader = require('./include/Uploader');
+var serveIndex = require('serve-index');
 var app = express();
+var uri= null;
+dns.lookup(require('os').hostname(), function (err, add, fam) {
+    console.log('addr: '+add);
+    QRCode.toDataURL('http://'+add+':'+port+'/root', function (err, url) {
+    uri = url;
+    console.log(url);
+    //opn('http://'+add+':'+port+'/barcode');
+  })
+  })
+app.use('/root',express.static(__dirname+'/uploads/'))
+app.use('/root',serveIndex(__dirname+'/uploads'))
+//setting up Templating Engine
+app.set('view engine','pug');
+app.set ('views','./views');
 app.get('/',(req,res)=>{
-    res.send("hope it works");
+    res.render('test');
 });
-app.listen(99999,()=>{console.log("listening")});
+app.get('/barcode',(req,res)=>{
+    res.render('barcode',{uri:uri})
+})
+//Uploading Handler
+app.post('/upload',(req,res)=>{
+    uploader(req,res,(code)=>{
+        console.log(code);
+        res.send(code);
+    })
+});
+app.listen(3000,()=>{
+    console.log("listening");
+});
